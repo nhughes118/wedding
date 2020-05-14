f58d3b6dfdd658456939026aa907cd57c95fec95
Nicholas Hughes <nkhughes@outlook.com>
2020-05-13 20:09:24 -0400

    fixed countdown on mobile

--- js/main.js
+++ js/main.js
@@ -25,59 +25,41 @@ $(document).ready(function(){
 	});
 
 	//countdown timer
+window.onload = function() {
+	  // Month Day, Year Hour:Minute:Second, id-of-element-container
+	  countDownToTime("Oct 23, 2020 17:00:00", 'countdown1'); // ****** Change this line!
+	}
+	function countDownToTime(countTo, id) {
+	  countTo = new Date(countTo).getTime();
+	  var now = new Date(),
+	      countTo = new Date(countTo),
+	      timeDifference = (countTo - now);
 
-	Vue.component('countdown', {
-	  template: '#countdown-template',
-	  mounted() {
-	    window.setInterval(() => {
-	        this.now = Math.trunc((new Date()).getTime() / 1000);
-	    },1000);
-	  },
-	  props: {
-	    date: {
-	      type: String
-	    }
-	  },
-	  data() {
-	    return {
-	      now: Math.trunc((new Date()).getTime() / 1000)
-	    }
-	  },
-	  computed: {
-	    dateInMilliseconds() {
-	      return Math.trunc(Date.parse(this.date) / 1000)
-	    },
-	    seconds() {
-	      return (this.dateInMilliseconds - this.now) % 60;
-	    },
-	    minutes() {
-	      return Math.trunc((this.dateInMilliseconds - this.now) / 60) % 60;
-	    },
-	    hours() {
-	      return Math.trunc((this.dateInMilliseconds - this.now) / 60 / 60) % 24;
-	    },
-	    days() {
-	      return Math.trunc((this.dateInMilliseconds - this.now) / 60 / 60 / 24);
-	    }
-	  }
-	});
+	  var secondsInADay = 60 *  60 * 1000 * 24,
+	      secondsInAHour = 60 * 60 * 1000;
 
-	Vue.filter('two_digits', (value) => {
-	  if (value < 0) {
-	    return '00';
-	  }
-	  if (value.toString().length <= 1) {
-	    return `0${value}`;
-	  }
-	  return value;
-	});
+	  days = Math.floor(timeDifference / (secondsInADay) * 1);
+	  hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+	  mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+	  secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+	  var idEl = document.getElementById(id);
+	  idEl.getElementsByClassName('days')[0].innerHTML = days;
+	  idEl.getElementsByClassName('hours')[0].innerHTML = hours;
+	  idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
+	  idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+
+	  clearTimeout(countDownToTime.interval);
+	  countDownToTime.interval = setTimeout(function(){ countDownToTime(countTo, id); },1000);
+	}
 
 
 
 
-	var app = new Vue({
-	  el: '#app'
-	});
+
+
+
+
 
 
 }); // closes my document.ready
